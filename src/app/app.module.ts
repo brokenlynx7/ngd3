f934d95c944872bc2579dd9879610535d5bbec01
darren.hamilton <darren.hamilton@accenture.com>
2018-03-29 09:57:44 +0100

    Various modularisation and linting updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,23 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { LineExampleComponent } from './components/line-example/line-example.component';
+import { GraphComponent } from './visuals/graph/graph/graph.component';
+import { NodeVisualComponent } from './visuals/node-visual/node-visual.component';
+import { LinkVisualComponent } from './visuals/link-visual/link-visual.component';
+import { ZoomableDirective } from './d3/directives/zoomable.directive';
+import { DraggableDirective } from './d3/directives/draggable.directive';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LineExampleComponent,
+    GraphComponent,
+    NodeVisualComponent,
+    LinkVisualComponent,
+    ZoomableDirective,
+    DraggableDirective
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/line-example/line-example.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: '[lineExample]',
+    template: `line-example.ts`
+})
+export class LineExampleComponent {
+    constructor() {}
+}
